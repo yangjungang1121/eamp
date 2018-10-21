920f164210b1cc255a1753ec4fc47746da0d7010
杨俊刚 <yangjungang@yn.csg.cn>
2018-10-21 17:09:22 +0800

    首次提交

---
+++ src/test/java/com/yjg/funly/eamp/EampApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yjg.funly.eamp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EampApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
